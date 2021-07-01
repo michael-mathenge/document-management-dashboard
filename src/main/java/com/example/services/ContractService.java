5d1ed64b351c7cde908c6b32527ca87dd669ed34
Mathenge <mmathenge1@gmail.com>
2021-07-01 09:20:12 +0300

    Update ContractService.java

--- src/main/java/com/example/services/ContractService.java
+++ src/main/java/com/example/services/ContractService.java
@@ -1,4 +1,4 @@
-package com.example.legal.services;
+package com.example.<package_name>.services;
 
 import com.example.legal.models.Contract;
 import com.example.legal.repositories.ContractRepository;
